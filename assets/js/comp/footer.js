25283499af17022cb23a3702376dae9441dbe34f
wali100z <adanwali501@gmail.com>
2025-09-10 13:14:34 +0200

    footer

---
+++ assets/js/comp/footer.js
@@ -0,0 +1,17 @@
+const footer = document.querySelector('footer');
+
+const footerHTML = `
+    <div class="footer-content">
+
+        <label for="name">First Name</label>
+        <input type="text" id="fname" name="firstname" placeholder="Your name..">
+
+        <label for="Gmail">Gmail</label>
+        <input type="text" id="gmail" name="gmail" placeholder="Your email..">
+
+        <label for="Besked">Besked</label>
+    <textarea id="Besked" name="Besked" placeholder="Write something.."></textarea>
+
+
+    </div>
+`;
